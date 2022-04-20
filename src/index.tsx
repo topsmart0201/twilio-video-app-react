4abe828e0a2a2b543d83950364829772ed840762
topsmart20 <oswellgrundmeier42@gmail.com>
2022-04-20 19:07:49 +0900

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { CssBaseline } from '@material-ui/core';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
+import App from './App';
+import AppStateProvider, { useAppState } from './state';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import ErrorDialog from './components/ErrorDialog/ErrorDialog';
+import generateConnectionOptions from './utils/generateConnectionOptions/generateConnectionOptions';
+import LoginPage from './components/LoginPage/LoginPage';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import theme from './theme';
+import './types';
+import { VideoProvider } from './components/VideoProvider';
+
+const VideoApp = () => {
+  const { error, setError, settings } = useAppState();
+  const connectionOptions = generateConnectionOptions(settings);
+
+  return (
+    <VideoProvider options={connectionOptions} onError={setError}>
+      <ErrorDialog dismissError={() => setError(null)} error={error} />
+      <App />
+    </VideoProvider>
+  );
+};
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    <Router>
+      <AppStateProvider>
+        <Switch>
+          <PrivateRoute exact path="/">
+            <VideoApp />
+          </PrivateRoute>
+          <PrivateRoute path="/room/:URLRoomName">
+            <VideoApp />
+          </PrivateRoute>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </AppStateProvider>
+    </Router>
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
